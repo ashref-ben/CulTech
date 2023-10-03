43ebbc2acee02a57f831c2b70d0947b79ab48511
KiiNooX <amir.zouenkhi@esprit.tn>
2023-10-03 14:49:39 +0100

    Event service

--- Event/Event/src/main/java/com/example/Event/Controller/EventController.java
+++ Event/Event/src/main/java/com/example/Event/Controller/EventController.java
@@ -1,19 +1,48 @@
 package com.example.Event.Controller;
 
+import com.example.Event.Entity.Events;
+import com.example.Event.Service.EventsService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/Event")
 public class EventController {
+    private final EventsService eventsService;
     @GetMapping("/hey")
     public String getAnonymous() {
         String ok="Im Event";
         System.out.println(ok);
         return ok;
     }
+    @GetMapping("/all")
+    public List<Events> getAll() {
+     return eventsService.getAll();
+    }
+
+    @PostMapping("/add")
+    public boolean add(@RequestBody Events events) {
+        return eventsService.add(events);
+    }
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable Long id) {
+        return eventsService.delete(id);
+    }
+    @PutMapping("/update")
+    public Boolean update(@RequestBody Events events) {
+        return eventsService.update(events);
+    }
+    @GetMapping("/getId")
+    public Long getId(@RequestBody Events events){
+        return eventsService.getId(events);
+    }
+    @GetMapping("/getEvent/{id}")
+    public Events getEvent(@PathVariable("id") Long id){
+        return eventsService.getEvent(id);
+    }
+
 }
